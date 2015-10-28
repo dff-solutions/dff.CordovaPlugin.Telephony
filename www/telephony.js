2e644a0c3ddd761c9c1fe7ef9d3f3bff7a00dd5a
Frank Koenigstein <frank.koenigstein@dff-solutions.de>
2015-10-28 16:25:48 +0100

    added www folder

---
+++ www/telephony.js
@@ -0,0 +1,34 @@
+/**
+ * JavaScript interface to abstract
+ * the usage of the cordova Sygic Navigation plugin.
+ *
+ * @module com/dff/cordova/plugins/sygic
+ */
+
+'use strict';
+
+var cordova = require('cordova');
+var feature = "Telephony";
+var self = {};
+
+/**
+ * Get call log.
+ *
+ */
+self.getCallsLog = function (success, error, args) {
+    cordova.exec(success, error, feature, "calllog", [args]);
+};
+
+self.call = function (success, error, args) {
+    cordova.exec(success, error, feature, "call", [args]);
+};
+
+self.onCallStateChanged = function (success, error) {
+    cordova.exec(success, error, feature, "onCallStateChanged", []);
+};
+
+self.onLog = function (success, error) {
+    cordova.exec(success, error, feature, "onLog", []);
+};
+
+module.exports = self;
