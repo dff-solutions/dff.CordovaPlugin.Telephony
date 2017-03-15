54b3d878f4ca770c75f9c6a90780baa2edda4860
Anthony Nahas <anthony.na@hotmail.de>
2017-03-15 15:54:42 +0100

    FEAT: @Target Android version starting with API 23: Requesting Permissions!

--- src/android/com/dff/cordova/plugin/telephony/TelephonyPlugin.java
+++ src/android/com/dff/cordova/plugin/telephony/TelephonyPlugin.java
@@ -1,21 +1,21 @@
 package com.dff.cordova.plugin.telephony;
 
-import java.lang.reflect.InvocationTargetException;
-
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.util.Log;
+import com.dff.cordova.plugin.common.CommonPlugin;
+import com.dff.cordova.plugin.common.action.CordovaAction;
+import com.dff.cordova.plugin.common.log.CordovaPluginLog;
+import com.dff.cordova.plugin.telephony.action.Call;
+import com.dff.cordova.plugin.telephony.action.ClearCallLog;
+import com.dff.cordova.plugin.telephony.action.GetCallLog;
+import com.dff.cordova.plugin.telephony.action.TelephonyInfo;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaInterface;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-import com.dff.cordova.plugin.common.CommonPlugin;
-import com.dff.cordova.plugin.common.action.CordovaAction;
-import com.dff.cordova.plugin.common.log.CordovaPluginLog;
-import com.dff.cordova.plugin.telephony.action.Call;
-import com.dff.cordova.plugin.telephony.action.GetCallLog;
-import com.dff.cordova.plugin.telephony.action.ClearCallLog;
-import com.dff.cordova.plugin.telephony.action.TelephonyInfo;
-
-import android.util.Log;
+import java.lang.reflect.InvocationTargetException;
 
 
 /**
@@ -24,103 +24,136 @@ import android.util.Log;
  * @author dff solutions
  */
 public class TelephonyPlugin extends CommonPlugin {
-	private static final String LOG_TAG = "com.dff.cordova.plugin.telephony.TelephonyPlugin";
-	
-	private TelephonyPhoneStateListener phoneStateListener;
-	
-	public TelephonyPlugin() {
-		super(LOG_TAG);
-		this.actions.put(TelephonyInfo.ACTION_NAME, TelephonyInfo.class);
-		this.actions.put(ClearCallLog.ACTION_NAME, ClearCallLog.class);
-		this.actions.put(GetCallLog.ACTION_NAME, GetCallLog.class);
-		this.actions.put(Call.ACTION_NAME, Call.class);
-	}
 
-   /**
-	* Called after plugin construction and fields have been initialized.
-	*/
-    public void pluginInitialize() {
-    	super.pluginInitialize();
-    	this.phoneStateListener = new TelephonyPhoneStateListener(this.cordova);
+    private static final String LOG_TAG = "com.dff.cordova.plugin.telephony.TelephonyPlugin";
+    private static final String[] PERMISSIONS =
+        {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_CONTACTS,
+            Manifest.permission.READ_CALL_LOG,
+            Manifest.permission.READ_PHONE_STATE,
+            Manifest.permission.CALL_PHONE,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        };
+
+    private static final int PERMISSION_CODE = 0;
+
+    private TelephonyPhoneStateListener phoneStateListener;
+
+    public TelephonyPlugin() {
+        super(LOG_TAG);
+        this.actions.put(TelephonyInfo.ACTION_NAME, TelephonyInfo.class);
+        this.actions.put(ClearCallLog.ACTION_NAME, ClearCallLog.class);
+        this.actions.put(GetCallLog.ACTION_NAME, GetCallLog.class);
+        this.actions.put(Call.ACTION_NAME, Call.class);
     }
-    
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (!cordova.hasPermission(PERMISSIONS[0])) {
+            getPermissions(PERMISSION_CODE);
+        } else if (!cordova.hasPermission(PERMISSIONS[6])) {
+            getPermissions(PERMISSION_CODE);
+        } else if (!cordova.hasPermission(PERMISSIONS[7])) {
+            getPermissions(PERMISSION_CODE);
+        }
+    }
+
+    private void getPermissions(int requestCode) {
+        cordova.requestPermissions(this, requestCode, PERMISSIONS);
+    }
+
+    public void onRequestPermissionResult(int requestCode, String[] permissions,
+                                          int[] grantResults) throws JSONException {
+        for (int r : grantResults) {
+            if (r == PackageManager.PERMISSION_DENIED) {
+                CordovaPluginLog.e(LOG_TAG, "PERMISSIONS DENIED");
+                return;
+            }
+        }
+    }
+
+    /**
+     * Called after plugin construction and fields have been initialized.
+     */
+    public void pluginInitialize() {
+        super.pluginInitialize();
+        this.phoneStateListener = new TelephonyPhoneStateListener(this.cordova);
+    }
+
     @Override
     public void onDestroy() {
-    	super.onDestroy();
-    	this.phoneStateListener.onDestroy();
+        super.onDestroy();
+        this.phoneStateListener.onDestroy();
     }
-       
+
     /**
-    * Executes the request.
-    *
-    * This method is called from the WebView thread.
-    * To do a non-trivial amount of work, use:
-    * cordova.getThreadPool().execute(runnable);
-    *
-    * To run on the UI thread, use:
-    * cordova.getActivity().runOnUiThread(runnable);
-    *
-    * @param action The action to execute.
-    * @param args The exec() arguments.
-    * @param callbackContext The callback context used when calling back into JavaScript.
-    * @return Whether the action was valid.
-    */
-	@Override
+     * Executes the request.
+     * <p>
+     * This method is called from the WebView thread.
+     * To do a non-trivial amount of work, use:
+     * cordova.getThreadPool().execute(runnable);
+     * <p>
+     * To run on the UI thread, use:
+     * cordova.getActivity().runOnUiThread(runnable);
+     *
+     * @param action          The action to execute.
+     * @param args            The exec() arguments.
+     * @param callbackContext The callback context used when calling back into JavaScript.
+     * @return Whether the action was valid.
+     */
+    @Override
     public boolean execute(String action
-    		, final JSONArray args
-    		, final CallbackContext callbackContext)
+        , final JSONArray args
+        , final CallbackContext callbackContext)
         throws JSONException {
-    	CordovaAction cordovaAction = null;
-		
-    	Log.d(LOG_TAG, "call for action: " + action + "; args: " + args);
-    	
-    	if (action.equals("onCallStateChanged")) {
-    		this.phoneStateListener.setOnCallStateChangedCallback(callbackContext);
-    		return true;
-    	}
-		else if (this.actions.containsKey(action)) {
-			Class<? extends CordovaAction> actionClass = this.actions.get(action);
+        CordovaAction cordovaAction = null;
 
-			Log.d(LOG_TAG, "found action: " + actionClass.getName());
+        Log.d(LOG_TAG, "call for action: " + action + "; args: " + args);
 
-			try {
-				cordovaAction = actionClass
-				        .getConstructor(
-				                String.class,
-				                JSONArray.class,
-				                CallbackContext.class,
-				                CordovaInterface.class)
-				        .newInstance(
-				                action,
-				                args,
-				                callbackContext,
-				                this.cordova);
-			}
-			catch (InstantiationException e) {
-				CordovaPluginLog.e(LOG_TAG, e.getMessage(), e);
-			}
-			catch (IllegalAccessException e) {
-				CordovaPluginLog.e(LOG_TAG, e.getMessage(), e);
-			}
-			catch (IllegalArgumentException e) {
-				CordovaPluginLog.e(LOG_TAG, e.getMessage(), e);
-			}
-			catch (InvocationTargetException e) {
-				CordovaPluginLog.e(LOG_TAG, e.getMessage(), e);
-			}
-			catch (NoSuchMethodException e) {
-				CordovaPluginLog.e(LOG_TAG, e.getMessage(), e);
-			}
-			catch (SecurityException e) {
-				CordovaPluginLog.e(LOG_TAG, e.getMessage(), e);
-			}
-		}
-    	
-    	if (cordovaAction != null) {
-    		this.cordova.getThreadPool().execute(cordovaAction);
+        if (action.equals("onCallStateChanged")) {
+            this.phoneStateListener.setOnCallStateChangedCallback(callbackContext);
             return true;
-    	}    	
+        } else if (this.actions.containsKey(action)) {
+            Class<? extends CordovaAction> actionClass = this.actions.get(action);
 
-    	return super.execute(action, args, callbackContext);
+            Log.d(LOG_TAG, "found action: " + actionClass.getName());
+
+            try {
+                cordovaAction = actionClass
+                    .getConstructor(
+                        String.class,
+                        JSONArray.class,
+                        CallbackContext.class,
+                        CordovaInterface.class)
+                    .newInstance(
+                        action,
+                        args,
+                        callbackContext,
+                        this.cordova);
+            } catch (InstantiationException e) {
+                CordovaPluginLog.e(LOG_TAG, e.getMessage(), e);
+            } catch (IllegalAccessException e) {
+                CordovaPluginLog.e(LOG_TAG, e.getMessage(), e);
+            } catch (IllegalArgumentException e) {
+                CordovaPluginLog.e(LOG_TAG, e.getMessage(), e);
+            } catch (InvocationTargetException e) {
+                CordovaPluginLog.e(LOG_TAG, e.getMessage(), e);
+            } catch (NoSuchMethodException e) {
+                CordovaPluginLog.e(LOG_TAG, e.getMessage(), e);
+            } catch (SecurityException e) {
+                CordovaPluginLog.e(LOG_TAG, e.getMessage(), e);
+            }
+        }
+
+        if (cordovaAction != null) {
+            this.cordova.getThreadPool().execute(cordovaAction);
+            return true;
+        }
+
+        return super.execute(action, args, callbackContext);
     }
 }
